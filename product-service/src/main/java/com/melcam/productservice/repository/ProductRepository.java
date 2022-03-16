bd541e15e97571fa78c1f0b96c441934958a427f
Rommeldev93 <yumelcamdev@gmail.com>
2022-03-15 19:01:05 -0500

    product service

---
+++ product-service/src/main/java/com/melcam/productservice/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.melcam.productservice.repository;
+
+import com.melcam.productservice.entity.Category;
+import com.melcam.productservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  public List<Product> findByCategory(Category category);
+}
